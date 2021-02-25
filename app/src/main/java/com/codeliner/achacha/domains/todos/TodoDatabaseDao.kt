8d50d0d9d6779704eb71a56a2c8f6b12f0e81df1
Codeliner <codeliner96@gmail.com>
2021-02-25 19:13:50 +0900

    Wrote Todo database DAO

---
+++ app/src/main/java/com/codeliner/achacha/domains/todos/TodoDatabaseDao.kt
@@ -0,0 +1,33 @@
+package com.codeliner.achacha.domains.todos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+interface TodoDatabaseDao {
+
+    // note. Create
+    @Insert
+    fun insert(todo: Todo)
+
+    // note. Read
+    @Query("SELECT * FROM todos_table ORDER BY id DESC")
+    fun getAllTodos(): LiveData<List<Todo>>
+
+    @Query("SELECT * FROM todos_table WHERE id = :todoId")
+    fun getTodoById(todoId: Long): Todo
+
+    fun getTodoLatest(): LiveData<Todo>
+
+    // note. Update
+    @Update
+    fun update(todo: Todo)
+
+    // note. Delete
+    @Query("DELETE FROM todos_table")
+    fun clear()
+
+    @Query("DELETE FROM todos_table WHERE id = :todoId")
+    fun deleteTodoById(todoId: Long)
+}
