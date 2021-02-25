092cb226442ccc83440a3de1bc4d04d904db3e01
Codeliner <codeliner96@gmail.com>
2021-02-25 19:20:56 +0900

    Wrote Todo database

---
+++ app/src/main/java/com/codeliner/achacha/domains/todos/TodoDatabase.kt
@@ -0,0 +1,37 @@
+package com.codeliner.achacha.domains.todos
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
+abstract class TodoDatabase: RoomDatabase() {
+
+    abstract val todoDatabaseDao: TodoDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoDatabase? = null
+
+        fun getInstance(context: Context): TodoDatabase{
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TodoDatabase::class.java,
+                        "todo_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
