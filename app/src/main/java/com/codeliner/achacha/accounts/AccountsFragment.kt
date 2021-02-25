b0c5732a4aaac04d97ee40abccec8e180b71fc97
Codeliner <codeliner96@gmail.com>
2021-02-25 18:56:51 +0900

    Added bottom navigation and items

---
+++ app/src/main/java/com/codeliner/achacha/accounts/AccountsFragment.kt
@@ -0,0 +1,25 @@
+package com.codeliner.achacha.accounts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codeliner.achacha.databinding.FragmentAccountsBinding
+
+class AccountsFragment: Fragment() {
+
+    private lateinit var binding: FragmentAccountsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAccountsBinding.inflate(inflater)
+
+
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/codeliner/achacha/mains/MainActivity.kt
+++ app/src/main/java/com/codeliner/achacha/mains/MainActivity.kt
@@ -3,15 +3,31 @@ package com.codeliner.achacha.mains
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.codeliner.achacha.R
 import com.codeliner.achacha.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var nc: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initialize()
     }
+
+    private fun initialize() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        nc = this.findNavController(R.id.activityMain_navHostFragment)
+
+        // note. for bottom navigation with nav controller
+        binding.activityMainBottomNav.setupWithNavController(nc)
+    }
+
+
 }
