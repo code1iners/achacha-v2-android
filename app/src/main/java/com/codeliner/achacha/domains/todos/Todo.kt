bf2f1ab9e4df6ed6d9fd2e277be85360a6edc715
Codeliner <codeliner96@gmail.com>
2021-02-25 19:08:58 +0900

    Wrote Todo entity

---
+++ app/src/main/java/com/codeliner/achacha/domains/todos/Todo.kt
@@ -0,0 +1,24 @@
+package com.codeliner.achacha.domains.todos
+
+import android.graphics.drawable.Drawable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todos_table")
+data class Todo (
+        @PrimaryKey(autoGenerate = true)
+        var id: Long = 0L,
+        @ColumnInfo(name = "created")
+        var created: Long = System.currentTimeMillis(),
+        @ColumnInfo(name = "icon")
+        var icon: Drawable? = null,
+        @ColumnInfo(name = "work")
+        var work: String? = null,
+        @ColumnInfo(name = "help")
+        var help: String? = null,
+        @ColumnInfo(name = "is_finished")
+        var isFinished: Boolean = false,
+        @ColumnInfo(name = "is_important")
+        var isImportant: Boolean = false
+)
