f6f6faf491463725c7350bcc0ab0696729d1bea5
Codeliner <codeliner96@gmail.com>
2021-03-13 13:51:05 +0900

    Added biometric authenticate

--- app/src/main/java/com/codeliner/achacha/ui/auths/AuthBioFragment.kt
+++ app/src/main/java/com/codeliner/achacha/ui/auths/AuthBioFragment.kt
@@ -10,6 +10,7 @@ import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.andrognito.patternlockview.PatternLockView
 import com.andrognito.patternlockview.listener.PatternLockViewListener
 import com.codeliner.achacha.databinding.FragmentAuthBioBinding
@@ -67,112 +68,134 @@ class AuthBioFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentAuthBioBinding.inflate(inflater)
-
-        // note. pattern
-        binding.patternView.addPatternLockListener(patternListener)
-
-        executor = ContextCompat.getMainExecutor(context)
-        biometricPrompt = BiometricPrompt(this, executor, object: BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-                Timber.w("Authentication error: $errorCode")
-                context?.toastingShort("$errString")
-
-                when (errorCode) {
-                    BiometricPrompt.ERROR_CANCELED -> {
-                        Timber.w("ERROR_CANCELED")
-                    }
-
-                    BiometricPrompt.ERROR_HW_NOT_PRESENT -> {
-                        Timber.w("ERROR_HW_NOT_PRESENT")
-                    }
-
-                    BiometricPrompt.ERROR_HW_UNAVAILABLE -> {
-                        Timber.w("ERROR_HW_UNAVAILABLE")
-                    }
-
-                    BiometricPrompt.ERROR_LOCKOUT -> {
-                        Timber.w("ERROR_LOCKOUT")
-                    }
-
-                    BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> {
-                        Timber.w("ERROR_LOCKOUT_PERMANENT")
-                    }
-
-                    BiometricPrompt.ERROR_NEGATIVE_BUTTON -> {
-                        Timber.w("ERROR_NEGATIVE_BUTTON")
-                    }
-
-                    BiometricPrompt.ERROR_NO_BIOMETRICS -> {
-                        Timber.w("ERROR_NO_BIOMETRICS")
-
-                        // note. using pattern lock
-                        binding.patternContainer.visibility = View.VISIBLE
-                    }
-
-                    BiometricPrompt.ERROR_NO_DEVICE_CREDENTIAL -> {
-                        Timber.w("ERROR_NO_DEVICE_CREDENTIAL")
-                    }
-
-                    BiometricPrompt.ERROR_NO_SPACE -> {
-                        Timber.w("ERROR_NO_SPACE")
-                    }
-
-                    BiometricPrompt.ERROR_TIMEOUT -> {
-                        Timber.w("ERROR_TIMEOUT")
-                    }
-
-                    BiometricPrompt.ERROR_UNABLE_TO_PROCESS -> {
-                        Timber.w("ERROR_UNABLE_TO_PROCESS")
-                    }
-
-                    BiometricPrompt.ERROR_USER_CANCELED -> {
-                        Timber.w("ERROR_USER_CANCELED")
-                    }
-
-                    BiometricPrompt.ERROR_VENDOR -> {
-                        Timber.w("ERROR_VENDOR")
-                    }
-                }
-            }
-
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                context?.toastingShort("Authentication succeeded")
-            }
-
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-                context?.toastingShort("Authentication failed")
-            }
-        })
-
-//        val deviceAPILevel = android.os.Build.VERSION.SDK_INT
-//        Timber.d("deviceAPILevel: $deviceAPILevel")
-//        when (deviceAPILevel > 29) {
-//            true -> {
-//
-//            }
-//
-//            false -> {
-//
-//            }
-//        }
-
-        promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Biometric login for my app")
-            .setSubtitle("Log in using your biometric credential")
-            .setNegativeButtonText("negative button text")
-            .build()
-
-        biometricPrompt.authenticate(promptInfo)
-
+        initialize(inflater)
+        biometricStart()
         return binding.root
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Timber.w("requestCode: $requestCode, resultCode: $resultCode, data: $data")
+    private fun initialize(inflater: LayoutInflater) {
+        binding = FragmentAuthBioBinding.inflate(inflater)
+
+        // note. pattern
+        initializeListeners()
+
+        initializeBiometric()
+    }
+
+    private fun initializeListeners() {
+        // note. pattern event listener
+        binding.patternView.addPatternLockListener(patternListener)
+        // note. biometric event listener
+        binding.biometricAgain.setOnClickListener {
+            biometricStart()
+        }
+        binding.usePattern.setOnClickListener {
+            binding.biometricContainer.visibility = View.GONE
+            binding.patternContainer.visibility = View.VISIBLE
+        }
+    }
+
+    private fun initializeBiometric() {
+        initializeExecutor()
+        initializeBiometricPrompt()
+        initializePromptInfo()
+    }
+
+    private fun initializeExecutor() {
+        executor = ContextCompat.getMainExecutor(context)
+    }
+
+    private fun initializeBiometricPrompt() {
+        biometricPrompt =
+                BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        Timber.w("Authentication error\ncode: $errorCode, message: $errString")
+                        //                context?.toastingShort("$errString")
+
+                        when (errorCode) {
+                            BiometricPrompt.ERROR_CANCELED -> {
+                                Timber.w("ERROR_CANCELED")
+                            }
+
+                            BiometricPrompt.ERROR_HW_NOT_PRESENT -> {
+                                Timber.w("ERROR_HW_NOT_PRESENT")
+                            }
+
+                            BiometricPrompt.ERROR_HW_UNAVAILABLE -> {
+                                Timber.w("ERROR_HW_UNAVAILABLE")
+                            }
+
+                            BiometricPrompt.ERROR_LOCKOUT -> {
+                                Timber.w("ERROR_LOCKOUT")
+                            }
+
+                            BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> {
+                                Timber.w("ERROR_LOCKOUT_PERMANENT")
+                            }
+
+                            BiometricPrompt.ERROR_NEGATIVE_BUTTON -> {
+                                Timber.w("ERROR_NEGATIVE_BUTTON")
+                                binding.biometricAgain.visibility = View.VISIBLE
+                                binding.usePattern.visibility = View.VISIBLE
+                            }
+
+                            BiometricPrompt.ERROR_NO_BIOMETRICS -> {
+                                Timber.w("ERROR_NO_BIOMETRICS")
+
+                                // note. using pattern lock
+                                binding.patternContainer.visibility = View.VISIBLE
+                            }
+
+                            BiometricPrompt.ERROR_NO_DEVICE_CREDENTIAL -> {
+                                Timber.w("ERROR_NO_DEVICE_CREDENTIAL")
+                            }
+
+                            BiometricPrompt.ERROR_NO_SPACE -> {
+                                Timber.w("ERROR_NO_SPACE")
+                            }
+
+                            BiometricPrompt.ERROR_TIMEOUT -> {
+                                Timber.w("ERROR_TIMEOUT")
+                            }
+
+                            BiometricPrompt.ERROR_UNABLE_TO_PROCESS -> {
+                                Timber.w("ERROR_UNABLE_TO_PROCESS")
+                            }
+
+                            BiometricPrompt.ERROR_USER_CANCELED -> {
+                                Timber.w("ERROR_USER_CANCELED")
+                            }
+
+                            BiometricPrompt.ERROR_VENDOR -> {
+                                Timber.w("ERROR_VENDOR")
+                            }
+                        }
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        Timber.w("Authentication succeeded")
+
+                        findNavController().navigate(AuthBioFragmentDirections.actionAuthBioFragmentToTodoListFragment())
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        Timber.w("Authentication failed")
+                    }
+                })
+    }
+
+    private fun initializePromptInfo() {
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("negative button text")
+                .build()
+    }
+
+    private fun biometricStart() {
+        biometricPrompt.authenticate(promptInfo)
     }
 }
