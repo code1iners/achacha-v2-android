59a23f01b43c3791a67029ec35b37a8e0067c598
Codeliner <codeliner96@gmail.com>
2021-03-22 13:38:47 +0900

    Apply tag list in chip input activity.

--- app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputActivity.kt
+++ app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputActivity.kt
@@ -2,9 +2,15 @@ package com.codeliner.achacha.ui.inputs.chip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.view.marginRight
 import androidx.databinding.DataBindingUtil
 import com.codeliner.achacha.R
 import com.codeliner.achacha.databinding.ActivityChipInputBinding
+import com.example.helpers.MeasureManager.toDp
+import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginBottom
+import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginHorizontal
+import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginVertical
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChipInputActivity : AppCompatActivity() {
@@ -15,11 +21,24 @@ class ChipInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialize()
-        viewModel.showTags()
     }
 
     private fun initialize() {
         initializeBinding()
+        initializeFlexItems()
+    }
+
+    private fun initializeFlexItems() {
+        viewModel.showTags()
+        viewModel.tags.forEach { item ->
+            val itemView = TextView(this)
+            itemView.text = item
+            itemView.setMarginHorizontal(4.toDp(this))
+            itemView.isClickable = true
+            itemView.setBackgroundResource(R.drawable.item_touch_effect)
+
+            binding.bodyFlexBox.addView(itemView)
+        }
     }
 
     private fun initializeBinding() {
--- app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputViewModel.kt
+++ app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputViewModel.kt
@@ -2,14 +2,13 @@ package com.codeliner.achacha.ui.inputs.chip
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
-import com.codeliner.achacha.R
 import com.codeliner.achacha.utils.getTags
 import timber.log.Timber
 
 class ChipInputViewModel(
     application: Application
 ): ViewModel() {
-    private val tags = application.getTags()
+    val tags = application.getTags()
 
     fun showTags() {
         tags.forEach { item ->
