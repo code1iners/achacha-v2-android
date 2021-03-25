2100e73cd398355fbfdfea96e06c773f86af5607
Codeliner <codeliner96@gmail.com>
2021-03-25 19:21:17 +0900

    Added clicked effect in ChipInputActivity.

--- app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputActivity.kt
+++ app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputActivity.kt
@@ -1,17 +1,19 @@
 package com.codeliner.achacha.ui.inputs.chip
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import com.codeliner.achacha.R
 import com.codeliner.achacha.databinding.ActivityChipInputBinding
+import com.codeliner.achacha.utils.getTags
 import com.example.helpers.MeasureManager.toDp
 import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginHorizontal
-import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginRight
 import com.example.helpers.WidgetManager.LayoutParamsManager.Companion.setMarginVertical
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
@@ -24,6 +26,7 @@ class ChipInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initialize()
+        observers()
     }
 
     private fun initialize() {
@@ -33,14 +36,19 @@ class ChipInputActivity : AppCompatActivity() {
 
     private fun initializeFlexItems() {
         for (item in viewModel.tags) {
-            // note. Add tag items.
+            // note. Add tag items with attributes..
             val itemView = TextView(this)
             itemView.text = item
             itemView.setMarginVertical(2.toDp(this))
             itemView.setPadding(8.toDp(this), 2.toDp(this), 8.toDp(this), 2.toDp(this))
             itemView.isClickable = true
-            itemView.setBackgroundResource(R.drawable.item_touch_effect_rounded_m)
-
+            itemView.setTextColor(ContextCompat.getColor(this, R.color.primaryTextColor))
+            itemView.setBackgroundResource(R.drawable.unselected_item_touch_effect_rounded_m)
+            itemView.setOnClickListener {
+                // note. Add or Remove in list When clicked chip item.
+                viewModel.tagAddOrRemove(it as TextView)
+            }
+            // note. Added child chip item in flex box.
             binding.bodyFlexBox.addView(itemView)
 
             // note. Add spacing between tag items.
@@ -55,4 +63,39 @@ class ChipInputActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
     }
 
+    private fun observers() {
+        observeTags()
+    }
+
+    private fun observeTags() {
+        viewModel.onSelectedTags.observe(this) {
+            it?.let { tags ->
+                retrieveSelectedItems(tags)
+                updateSelectedItemUI()
+            }
+        }
+    }
+
+    private fun retrieveSelectedItems(tags: ArrayList<TextView>) {
+        binding.bodyFlexBox.forEachIndexed { index, flexView ->
+            // note. Convert view type.
+            val flexItem = flexView as TextView
+            // note. Check item is tag or spacer.
+            if (!flexItem.text.isNullOrEmpty()) {
+                // note. Clear all tags text style by default.
+                flexItem.setTypeface(null, Typeface.NORMAL)
+                flexItem.setTextColor(ContextCompat.getColor(this, R.color.primaryTextColor))
+                // note. Add selected item position.
+                if (tags.contains(flexItem)) viewModel.selectItem(index)
+            }
+        }
+    }
+
+    private fun updateSelectedItemUI() {
+        for (selectedItem in viewModel.selectedItems) {
+            val flexItem = binding.bodyFlexBox[selectedItem] as TextView
+            flexItem.setTypeface(null, Typeface.BOLD)
+            flexItem.setTextColor(ContextCompat.getColor(this, R.color.sexyBlue))
+        }
+    }
 }
--- app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputViewModel.kt
+++ app/src/main/java/com/codeliner/achacha/ui/inputs/chip/ChipInputViewModel.kt
@@ -1,6 +1,9 @@
 package com.codeliner.achacha.ui.inputs.chip
 
 import android.app.Application
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.codeliner.achacha.utils.getTags
 import timber.log.Timber
@@ -9,6 +12,30 @@ class ChipInputViewModel(
     application: Application
 ): ViewModel() {
     val tags = application.getTags()
+
+    private val _onSelectedTags = MutableLiveData(ArrayList<TextView>())
+    val onSelectedTags: LiveData<ArrayList<TextView>> get() = _onSelectedTags
+    fun tagAddOrRemove(tag: TextView) {
+        onSelectedTags.value?.let { tags ->
+            // note. Clear to match items positioning.
+            selectedItems.clear()
+            // note. Check tags already exists in list.
+            val newList = tags
+            when (tags.contains(tag)) {
+                true -> newList.remove(tag)
+                false -> newList.add(tag)
+            }
+            // note. Update list by new list.
+            _onSelectedTags.value = newList
+        }
+    }
+
+    val selectedItems = ArrayList<Int>()
+    fun selectItem(itemPosition: Int) {
+        if (!selectedItems.contains(itemPosition)) {
+            selectedItems.add(itemPosition)
+        }
+    }
 
     fun showTags() {
         tags.forEach { item ->
